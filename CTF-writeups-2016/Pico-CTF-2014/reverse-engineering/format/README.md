fc158b257a12ec3be9c5c8c20ee915084862c4e2
invinciblewolf <vjprakash0@gmail.com>
2016-11-11 13:11:23 -0500

    test commit for writeups

---
+++ CTF-writeups-2016/Pico-CTF-2014/reverse-engineering/format/README.md
@@ -0,0 +1,25 @@
+# Pico-CTF-2014 write-ups
+
+* <TODO>
+* [Scoreboard](TODO) or [local alternative](TODOLOCAL)
+
+## Completed write-ups
+
+* none yet
+
+## External write-ups only
+
+* none yet
+
+## Missing write-ups
+
+* [reverse-engineering/format/](reverse-engineering/format/)ck! See if you can modify a variable by supplying a format string! The binary can be found at /home/format/ on the shell server. The source can be found here.
+
+
+## Write-up
+
+(TODO)
+
+## Other write-ups and resources
+
+* none yet
---
+++ CTF-writeups-2016/Pico-CTF-2014/reverse-engineering/format/test_format_string.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include  <string.h>
+#include  <stdlib.h>
+
+int main (int argc, char **argv)
+{
+	char buf [100];
+	int x = 1 ; 
+	snprintf ( buf, sizeof buf, argv [1] ) ;
+	buf [ sizeof buf -1 ] = 0;
+	printf ( "Buffer size is: (%d) \nData input: %s \n" , strlen (buf) , buf ) ;
+	printf ( "X equals: %d/ in hex: %#x\nMemory address for x: (%p) \n" , x, x, &x) ;
+	int hour,min,sec,precision;
+	hour=5;
+	min=55;
+	sec=32;
+	precision=3;
+	printf("%2$d:%2$.*3$d:%4$.*3$d\n", hour, min, precision, sec);
+	return 0 ;
+}
